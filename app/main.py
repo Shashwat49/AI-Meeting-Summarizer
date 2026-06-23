680e0f808bcc1dfd5e27a24ae8a33b477cf4084d
Shashwat Shivam <91746469+Shashwat49@users.noreply.github.com>
2026-06-23 15:07:38 +0530

    Updated main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.database import Base, engine
-from app.routers import auth, meetings, projects
+from app.routers import auth, meetings, projects, chat
 
 Base.metadata.create_all(bind=engine)
 
@@ -23,6 +23,7 @@ app.add_middleware(
 app.include_router(auth.router)
 app.include_router(projects.router)
 app.include_router(meetings.router)
+app.include_router(chat.router)
 
 
 @app.get("/")
